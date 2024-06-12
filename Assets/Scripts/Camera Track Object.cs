2fb82c086cc62ec8bd9f1f77a2a9c08d8f119825
Barry Small <barry.small154@gmail.com>
2024-06-12 15:33:38 +0200

    Added vehicle hover and movement

---
+++ Assets/Scripts/Camera Track Object.cs
@@ -0,0 +1,30 @@
+// Tutorial: https://www.youtube.com/watch?v=r9OEZmbD9q0
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrackObject : MonoBehaviour
+{
+    public Transform target;
+    public float distanceUP;
+    public float distanceBACK;
+    public float minHeight;
+
+    // Not used directly
+    private Vector3 posVelocity;
+
+    private void FixedUpdate()
+    {
+        // Calculate a new position to place the camera
+        Vector3 newPos = target.position + (target.forward * distanceBACK);
+        newPos.y = Mathf.Max(newPos.y + distanceUP, minHeight);
+
+        // Move camera
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref posVelocity, 0.18f);
+
+        // Rotate the camera to keep the vehicle in view
+        Vector3 focalPoint = target.position + (target.forward * 5);
+        transform.LookAt(focalPoint);
+    }
+}
